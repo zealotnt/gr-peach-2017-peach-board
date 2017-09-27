325de820df92898917005d69053d1708ff3ade95
Tran Tam <tranminhtam.10192@gmail.com>
2017-09-27 09:03:27 +0700

    add utility to put misc things here

--- source/main-http.cpp
+++ source/main-http.cpp
@@ -28,7 +28,7 @@
 #include "USBHostMSD.h"
 #include "usb_host_setting.h"
 #include "dec_wav.h"
-#include "dev_wav_http.h"
+#include "grUtility.h"
 
 #define USB_HOST_CH     1
 #define DUMP_HTTP_BODY 1
@@ -70,36 +70,6 @@ void dump_response(HttpResponse* res) {
         printf("\t%s: %s\n", res->get_headers_fields()[ix]->c_str(), res->get_headers_values()[ix]->c_str());
     }
     printf("\nBody (%d bytes):\n\n%s\n", res->get_body_length(), res->get_body_as_string().c_str());
-}
-
-// [Ref-Source](https://gist.github.com/ccbrown/9722406)
-void DumpHex(const void* data, size_t size) {
-    char ascii[17];
-    size_t i, j;
-    ascii[16] = '\0';
-    for (i = 0; i < size; ++i) {
-        printf("%02X ", ((unsigned char*)data)[i]);
-        if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
-            ascii[i % 16] = ((unsigned char*)data)[i];
-        } else {
-            ascii[i % 16] = '.';
-        }
-        if ((i+1) % 8 == 0 || i+1 == size) {
-            printf(" ");
-            if ((i+1) % 16 == 0) {
-                printf("|  %s \n", ascii);
-            } else if (i+1 == size) {
-                ascii[(i+1) % 16] = '\0';
-                if ((i+1) % 16 <= 8) {
-                    printf(" ");
-                }
-                for (j = (i+1) % 16; j < 16; ++j) {
-                    printf("   ");
-                }
-                printf("|  %s \n", ascii);
-            }
-        }
-    }
 }
 
 static void on_body_cb(const char *at, size_t length)
